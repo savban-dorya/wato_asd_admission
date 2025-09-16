97952ff308fd5dda9648aeb738761aa7ad556a1d
savban-dorya <sdorya776@gmail.com>
2025-09-15 23:35:01 -0400

    Got to Planner Node Step 3

--- src/robot/map_memory/include/map_memory_node.hpp
+++ src/robot/map_memory/include/map_memory_node.hpp
@@ -2,7 +2,8 @@
 #define MAP_MEMORY_NODE_HPP_
 
 #include "rclcpp/rclcpp.hpp"
-
+#include "nav_msgs/msg/occupancy_grid.hpp"
+#include "nav_msgs/msg/odometry.hpp"
 #include "map_memory_core.hpp"
 
 class MapMemoryNode : public rclcpp::Node {
@@ -11,6 +12,28 @@ class MapMemoryNode : public rclcpp::Node {
 
   private:
     robot::MapMemoryCore map_memory_;
+    nav_msgs::msg::OccupancyGrid occupancy_grid_;
+    nav_msgs::msg::Odometry odom_;
+
+    //publisher
+    rclcpp::Publisher<nav_msgs::msg::OccupancyGrid>::SharedPtr occupancy_grid_pub_;
+
+    //subscriptions
+    rclcpp::Subscription<nav_msgs::msg::OccupancyGrid>::SharedPtr costmap_sub_;
+    rclcpp::Subscription<nav_msgs::msg::Odometry>::SharedPtr odom_sub_;
+
+    //timer
+    rclcpp::TimerBase::SharedPtr timer_;
+
+    //functions
+    void costmapCallback(const nav_msgs::msg::OccupancyGrid::SharedPtr msg);
+    void odomCallback(const nav_msgs::msg::Odometry::SharedPtr msg);
+    void publishMapMemory();
+
+    //variables
+    double last_x_, last_y_;
+    const double DISTANCE_THRESHOLD = 0.5; // meters
+    bool costmap_updated_ = false;
 };
 
 #endif 
--- src/robot/map_memory/src/map_memory_node.cpp
+++ src/robot/map_memory/src/map_memory_node.cpp
@@ -1,6 +1,32 @@
 #include "map_memory_node.hpp"
 
-MapMemoryNode::MapMemoryNode() : Node("map_memory"), map_memory_(robot::MapMemoryCore(this->get_logger())) {}
+MapMemoryNode::MapMemoryNode() : Node("map_memory"), map_memory_(robot::MapMemoryCore(this->get_logger())) {
+
+  // Initialize subscriber to costmap
+  costmap_sub_ = this->create_subscription<nav_msgs::msg::OccupancyGrid>(
+      "/costmap", 10, std::bind(&MapMemoryNode::costmapCallback, this, std::placeholders::_1));
+
+  // Initialize subscriber to odometry
+  odom_sub_ = this->create_subscription<nav_msgs::msg::Odometry>(
+            "/odom/filtered", 10, std::bind(&MapMemoryNode::odomCallback, this, std::placeholders::_1));
+
+  // Initialize timer to check distances
+  timer_ = this->create_wall_timer(std::chrono::milliseconds(1000), std::bind(&MapMemoryNode::publishMapMemory, this));
+
+
+}
+
+void MapMemoryNode::costmapCallback(const nav_msgs::msg::OccupancyGrid::SharedPtr msg) {
+  // Store the received costmap as a pointer
+  occupancy_grid_ = *msg;
+  RCLCPP_INFO(this->get_logger(), "Received new costmap.");
+}
+
+void MapMemoryNode::odomCallback(const nav_msgs::msg::Odometry::SharedPtr msg) {
+  // Store the received odometry as a pointer
+  odom_ = *msg;
+  RCLCPP_INFO(this->get_logger(), "Received new odometry.");
+}
 
 int main(int argc, char ** argv)
 {

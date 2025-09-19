6762e063417af29f276313a10898fc689b5b9dc9
savban-dorya <sdorya776@gmail.com>
2025-09-19 15:39:34 -0400

    Reset Planner

--- src/robot/planner/include/planner_node.hpp
+++ src/robot/planner/include/planner_node.hpp
@@ -4,12 +4,6 @@
 #include "rclcpp/rclcpp.hpp"
 
 #include "planner_core.hpp"
-// Need to include relevant header files for accessing navigation and geometry messages that
-// you subscribed to or are publishing to
-#include "nav_msgs/msg/occupancy_grid.hpp"
-#include "nav_msgs/msg/odometry.hpp"
-#include "nav_msgs/msg/path.hpp"
-#include "geometry_msgs/msg/point_stamped.hpp"
 
 class PlannerNode : public rclcpp::Node {
   public:
@@ -17,57 +11,6 @@ class PlannerNode : public rclcpp::Node {
 
   private:
     robot::PlannerCore planner_;
-    rclcpp::Subscription<nav_msgs::msg::OccupancyGrid>::SharedPtr occupancy_grid_sub_;
+};
 
-};
-// ------------------- Supporting Structures -------------------
- 
-// 2D grid index
-struct CellIndex
-{
-  int x;
-  int y;
- 
-  CellIndex(int xx, int yy) : x(xx), y(yy) {}
-  CellIndex() : x(0), y(0) {}
- 
-  bool operator==(const CellIndex &other) const
-  {
-    return (x == other.x && y == other.y);
-  }
- 
-  bool operator!=(const CellIndex &other) const
-  {
-    return (x != other.x || y != other.y);
-  }
-};
- 
-// Hash function for CellIndex so it can be used in std::unordered_map
-struct CellIndexHash
-{
-  std::size_t operator()(const CellIndex &idx) const
-  {
-    // A simple hash combining x and y
-    return std::hash<int>()(idx.x) ^ (std::hash<int>()(idx.y) << 1);
-  }
-};
- 
-// Structure representing a node in the A* open set
-struct AStarNode
-{
-  CellIndex index;
-  double f_score;  // f = g + h
- 
-  AStarNode(CellIndex idx, double f) : index(idx), f_score(f) {}
-};
- 
-// Comparator for the priority queue (min-heap by f_score)
-struct CompareF
-{
-  bool operator()(const AStarNode &a, const AStarNode &b)
-  {
-    // We want the node with the smallest f_score on top
-    return a.f_score > b.f_score;
-  }
-};
 #endif 
